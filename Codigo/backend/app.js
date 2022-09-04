4d0f59c738c53360146421cf5a01713350c91583
Nikolas Louret <navlouret@sga.pucminas.br>
2022-09-04 16:53:28 -0300

    [Update] Adição de comentários

--- Codigo/backend/app.js
+++ Codigo/backend/app.js
@@ -19,11 +19,12 @@ app.use(express.json())
 app.use('/api', apiRoutes);
 app.use('/cadastrarProduto', cadastrarProdutoRoute);
 
-// Porta do app
+// Acesso ao banco de dados
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 const DB_STRING = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@atelier.unn2ute.mongodb.net/bancoAPI?retryWrites=true&w=majority`
 
+// Porta do app
 mongoose.connect(DB_STRING).then(() => {
     console.log("Banco de dados conectado")
     console.log("Servidor conectado")
--- Codigo/backend/routes/apiRoutes.js
+++ Codigo/backend/routes/apiRoutes.js
@@ -2,19 +2,27 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-// Controlers
+// Controllers
 const controller = require('../controller/inventoryController')
 
 router.get('/', (req, res) => {
     res.json({"message": "Teste das rotas da api do express"})
 })
 
+//#region rotas CRUD
+
+// Rotas GET
 router.get('/viewAllInventory', controller.viewAllInventory)
 router.get('/viewInventoryById', controller.viewInventoryById)
 
+// Rota POST
 router.post('/inventoryResgister', controller.inventoryResgister)
 
+// Rota PATCH
 router.patch('/updateInventory', controller.updateInventory)
 
+// Rota DELETE
 router.delete('/deleteInventory', controller.deleteInventory)
+//#endregion
+
 module.exports = router
