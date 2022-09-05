16c718d97c35ce2492dc01d7317408350120b757
GabrieldeSouza1 <gabriel.souza.1365691@sga.pucminas.br>
2022-09-05 19:39:17 -0300

    {Update} CadastrarProduto

--- Codigo/atelier_picinin/src/pages/jsx/CadastrarProduto.jsx
+++ Codigo/atelier_picinin/src/pages/jsx/CadastrarProduto.jsx
@@ -8,62 +8,97 @@ import Button from '../../components/Button'
 import Form from '../../components/FormCadastroProdutos'
 
 const CadastrarProduto = () => {
-    function deleteProduct(e){
-        const btn = e.target
-        var element = btn.parentNode
-        while(element.id == false)
-            element = element.parentNode
 
-    }
 
-    // Array com as colunas do cabeçalhos
-    const arrayHeader = ["PRODUTO", "SABOR", "PREÇO", "PEDIDO MÍNIMO", ""] // [coluna1, coluna2, coluna3...]
+    const [teste, setTest] = useState([[]]);
 
-    // Array com as propriedade dos objetos
-    const arrayProperties = ["nomeProduto"] // [coluna1, coluna2, coluna3...]
 
-    const [produtos, setProdutos] = useState([])
 
     useEffect(() => {
-        fetch('http://localhost:3000/products/viewAllProducts', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(resp => resp.json())
-        .then(data => setProdutos(data))
-        .catch(err => console.error(err))
-    })
+        setTimeout(() => {
+            fetch('http://localhost:3000/produto', {
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+    }).then((resp)=> resp.json())
+    .then((data)=>{
+        console.log(data)
+        setTest(data)
+        
+    }).catch((err)=>console.log(err))
+        },100)
+    },[])
+
+
+
+    
 
     return (
         <div className="body">
             <h1 className="title">Cadastro de Produto</h1>
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Inserir Novo Produto
             </button>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Cadastrando Novo Produto</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Cadastrando Novo Produto</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <Form id="form" action="" method="" btnText="Cadastrar" classNameButton="cadastrar"/>
+                            <form id="form" method='post' action='http://localhost:3000/produto/'>
+                                <label htmlFor="nome">Nome do Produto:</label>
+                                <input type="text" name="nome" id="nome" />
+                                <label htmlFor="descricao">Descrição do Produto:</label>
+                                <input type="text" name="descricao" id="descricao" />
+                                <label htmlFor="sabores">Sabor:</label>
+                                <input type="text" name="sabor" id="sabor" />
+                                <label htmlFor="preco">Preço:</label>
+                                <input type="number" name="preco" id="preco" min="0" />
+                                <label htmlFor="pedidoMinimo">Pedido Mínimo:</label>
+                                <input type="number" name="pedidoMinProduto" id="pedidoMinProduto" min="0" /><button type="reset" className='btnMais' id="btnMais"><AiOutlinePlus /></button>
+                                <label htmlFor="foto1">Foto de capa:</label>
+                                <input type="file" name="foto1" id="foto1" />
+                                <label htmlFor="foto2">Segunda foto:</label>
+                                <input type="file" name="foto2" id="foto2" />
+                                <label htmlFor="foto3">Terceira foto:</label>
+                                <input type="file" name="foto3" id="foto3" />
+                                <button type="submit" class="btn btn-warning " id='cadastrar' >Cadastrar</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
+            <table id='tabela'>
+                <thead>
+                    <td><span>PRODUTO</span></td>
+                    <td><span>SABOR</span></td>
+                    <td><span>PREÇO</span></td>
+                    <td><span>PEDIDO MÍNIMO</span></td>
+                    <td></td>
+                </thead>
 
-            <Tables arrayHeader={arrayHeader}
-                itens={produtos}
-                arrayProperties={arrayProperties}
-                textButton={
-                    <Button type="button" text={<BiTrash />}
-                    className="btnLixeira"
-                    event={deleteProduct} />
-                    }
-            />
+                
+                   
+                {
+                    teste.map(number =>
+                        <tr>
+                            <td>{number.nomeProduto}</td>
+                            <td>{number.saborProduto}</td>
+                            <td>{number.precoProduto}</td>
+                            <td>{number.pedidoMinProduto}</td>
+                            <td></td>
+                        </tr>                
+                    )
+                 }
+                  
+                
+                    
+                
+                
+            </table> 
         </div>
     )
 }
