127b9853f137b878615044ee3db4ca4e96c7f15e
Nikolas Louret <navlouret@sga.pucminas.br>
2022-09-12 20:47:02 -0300

    [Update] Adição de msg de insumo não encontrado

--- Codigo/atelier_picinin/src/components/Inventory/SuppliesTable.jsx
+++ Codigo/atelier_picinin/src/components/Inventory/SuppliesTable.jsx
@@ -1,29 +1,42 @@
+//#region imports
 import styles from '../css_components/layout/Tables.module.css'
 
 import CollapseElement from '../layout/CollapseElement'
 import InputComponentTable from './InputComponentTable'
 import InputCardMobile from './InputCardMobile'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+//#endregion
 
 const TableIsumo = ({ itens, buttonClickEvent, categorias, filterDropdownParams, filterSearchParams }) => {
+    //#region attr and configs
     const larguraTela = window.innerWidth
 
     const initialArray = []
-    for(let i = 0; i < categorias.length; i++)
-        initialArray.push(false)
+    categorias.forEach(() => initialArray.push(false))
 
+    const [itemFound, setItemFound] = useState(true)
     const [dropdown, setDropdown] = useState(initialArray)
 
-    function primeiraLetraMaiuscula(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    
     function dropdownConfig({ index }) {
         dropdown[index] = !dropdown[index]
 
         setDropdown(array => [...array])
     }
+    //#endregion
 
+    //#region hooks
+    useEffect(() => {
+        let result = itens.find(item => includeValues(item, ["name", "categoria"], filterSearchParams))
+
+        if(result)
+            setItemFound(true)
+        else
+            setItemFound(false)
+
+    }, [[], filterSearchParams])
+    //#endregion
+
+    //#region aux functions
     function includeValues(object, arrayAttribute, value) {
         let count = 0
         arrayAttribute.forEach(attribute => {
@@ -36,6 +49,12 @@ const TableIsumo = ({ itens, buttonClickEvent, categorias, filterDropdownParams,
         return count
     }
 
+    function primeiraLetraMaiuscula(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    //#endregion
+
+    //#region filter
     function searchStatusInItems(supplies){
         if(filterDropdownParams != "")
             return supplies.filter(input => input.status == filterDropdownParams)
@@ -52,10 +71,12 @@ const TableIsumo = ({ itens, buttonClickEvent, categorias, filterDropdownParams,
 
     function searchFilter(supplies) {
         let result = searchStatusInItems(supplies)
-        return searchNameCategory(result)
+        result = searchNameCategory(result)
+
+        return result
     }
 
-    function categoryFilter(categories){
+    function categoryFilter(categories) {
         return categories.filter(category => {
             const foundItem = searchFilter(itens).find(item => item.categoria == category)
 
@@ -63,16 +84,20 @@ const TableIsumo = ({ itens, buttonClickEvent, categorias, filterDropdownParams,
                 return category
         })
     }
+    //#endregion
     
-    return(
+    return (
         <>
             <div className={styles.table}>
                 <div className='body-table'>
                     {
-                        categoryFilter(categorias).map((categoria, index) =>
-                            <CollapseElement isOpened={dropdown[index]}
+                        !itemFound ? <p>Nenhum item encontrado</p>
+                        :
+                        categoryFilter(categorias).map((categoria, index) => (
+                            <CollapseElement isOpened={ dropdown[index] }
                                 buttonClickEvent={() => dropdownConfig({ index: index })}
                                 text={primeiraLetraMaiuscula(categoria)}>
+
                                 { larguraTela  >= 700 &&
                                     <div className={styles.headerTable}>
                                         <em key="insumo">INSUMO</em>
@@ -92,17 +117,17 @@ const TableIsumo = ({ itens, buttonClickEvent, categorias, filterDropdownParams,
                                         else
                                             return true
                                     })
-                                    .map(item => 
+                                    .map(item => (
                                         categoria == item.categoria && (
                                             larguraTela  >= 700 ? 
                                                 <InputComponentTable item={item} buttonClickEvent={buttonClickEvent} categories={categorias} />
                                             :
                                                 <InputCardMobile item={item} buttonClickEvent={buttonClickEvent} categories={categorias} />
                                         )
-                                    )
+                                    ))
                                 }
                             </CollapseElement>
-                        )
+                        ))
                     }
                 </div>
             </div>
