51db7fafea25a89b24ef5560fee8654f7c56d28d
Nikolas Louret <navlouret@sga.pucminas.br>
2022-09-21 18:04:57 -0300

    [Update] Finalização do carrinho

--- Codigo/atelier_picinin/src/components/layout/LinkButton.jsx
+++ Codigo/atelier_picinin/src/components/layout/LinkButton.jsx
@@ -3,7 +3,7 @@ import '../css_components/layout/Button.module.css'
 import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 
-function LinkButton({ to, state, type, text, classNameButton }) {
+function LinkButton({ to, state, type, children, classNameButton }) {
     const navigate = useNavigate()
 
     function clickEvent(){
@@ -14,7 +14,7 @@ function LinkButton({ to, state, type, text, classNameButton }) {
         className={classNameButton}
         buttonClickEvent={clickEvent}>
 
-        {text}
+        {children}
     </Button>
 }
 
--- Codigo/atelier_picinin/src/pages/jsx/Carrinho.jsx
+++ Codigo/atelier_picinin/src/pages/jsx/Carrinho.jsx
@@ -1,12 +1,32 @@
 import '../css/cart/Carrinho.css'
 
 import Cabecalho from '../../components/layout/CabecalhoCliente'
+import Loading from '../../components/layout/Loading'
 import CartItem from '../../components/cart/CartItem'
-import { useState } from 'react'
+import SummaryOrder from '../../components/cart/SummaryOrder'
+
+import { useState, useEffect } from 'react'
 
 const Carrinho = () => {
-    const [cartItems, setCartItems] = useState([{ _id: 1111, nome: "Suspiro", sabores: ["sal", "chocolate"], preco: 12.90, quantidade: 6 },
-            { _id: 2222, nome: "Goiabada", sabores: ["goiaba", "morango"], preco: 5.60, quantidade: 2 }])
+    const mockup = [{ _id: 1111, img: "http://via.placeholder.com/150.png", nome: "Suspiro", sabores: ["sal", "chocolate"], preco: 12.90, quantidade: 6, precoTotal: 77.40 },
+    { _id: 2222, img: "http://via.placeholder.com/150.png", nome: "Goiabada", sabores: ["goiaba", "morango"], preco: 6.50, quantidade: 2, precoTotal: 11.20 }]
+    //window.localStorage.setItem("user-cart", JSON.stringify(mockup))
+    
+    const data = window.localStorage.getItem("user-cart")
+    const [cartItems, setCartItems] = useState(data ? JSON.parse(data) : [])
+    const [isLoading, setIsLoading] = useState(true)
+    const [subtotal, setSubtotal] = useState(0)
+    const [entrega, setEntrega] = useState(0)
+    
+    useEffect(() => { setTimeout(() => setIsLoading(false), 600) }, [])
+    
+    useEffect(() => {
+        var subtotal = 0
+        cartItems.forEach(item => subtotal += item.precoTotal)
+        setSubtotal(subtotal.toFixed(2))
+
+        window.localStorage.setItem("user-cart", JSON.stringify(cartItems))
+    }, [cartItems])
 
     function findCartItem(id) {
         return cartItems.find(item => item._id == id) 
@@ -24,20 +44,29 @@ const Carrinho = () => {
 
     function addQuantity(e) {
         const id = e.target.id
+        const cartItem = findCartItem(id)
+
+        const editedCart = editCartItem(id, "quantidade", cartItem.quantidade + 1)
+        editCartItem = editCartItem(id, "precoTotal", +((cartItem.quantidade * cartItem.preco).toFixed(2)))
         
-        setCartItems(editCartItem(id, "quantidade", findCartItem(id).quantidade + 1))
+        setCartItems(editedCart)
     }
     
     function decrementQuantity(e) {
         const id = e.target.id
         const cartItem = findCartItem(id)
 
-        if(cartItem.quantidade > 1)
-            setCartItems(editCartItem(id, "quantidade", cartItem.quantidade - 1))
+        if(cartItem.quantidade > 1) {
+            const editedCart = editCartItem(id, "quantidade", cartItem.quantidade - 1)
+            editCartItem = editCartItem(id, "precoTotal", +((cartItem.quantidade * cartItem.preco).toFixed(2)))
+
+            setCartItems(editedCart)
+        }
     }
 
     function removeCartItem(e) {
-        setCartItems(cartItems.filter(item => item._id != e.target.id))
+        const id = e.target.id ? e.target.id : e.target.parentNode.id
+        setCartItems(cartItems.filter(item => item._id != id))
     }
 
     return (
@@ -53,16 +82,29 @@ const Carrinho = () => {
                     </div>
 
                     <div className="cart-items">
-                        { cartItems.map(cartItem => <CartItem content={cartItem} addEvent={addQuantity} removeEvent={decrementQuantity} removeItem={removeCartItem} /> ) }
+                    { isLoading ?
+                        <Loading />
+                        :
+                        cartItems.length ?
+                            cartItems.map(cartItem =>
+                                <CartItem content={cartItem}
+                                    addEvent={addQuantity}
+                                    removeEvent={decrementQuantity}
+                                    removeItem={removeCartItem} />
+                            )
+                        :
+                        <p className="cart-empty">O carrinho está vazio</p>
+                    }
                     </div>
                 </div>
 
                 <div className="summary-order">
                     <h1>Resumo do pedido</h1>
-
-                    <div className="order-price">
-                        
-                    </div>
+                    { isLoading ?
+                        <Loading />
+                        :
+                        <SummaryOrder subtotal={subtotal} entrega={entrega}/>
+                    }
                 </div>
             </div>
         </>
