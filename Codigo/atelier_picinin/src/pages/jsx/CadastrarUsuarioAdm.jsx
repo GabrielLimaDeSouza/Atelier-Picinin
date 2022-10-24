9af61d5239864f8868da96f2f039f88110067c71
Gabriel Lima de Souza <gabriel.souza.1354648@sga.pucminas.br>
2022-10-24 09:15:41 -0300

    [UPDATE] Responsividade cadastro

--- Codigo/atelier_picinin/src/pages/jsx/CadastrarUsuarioAdm.jsx
+++ Codigo/atelier_picinin/src/pages/jsx/CadastrarUsuarioAdm.jsx
@@ -8,7 +8,7 @@ const cadastarUsuarioAdm = () => {
     const [message, setMessage] = useState('')
     const [typeMessage, setTypeMessage] = useState('')
     var tamanho
-const url = 'http://localhost:3000'
+    const url = 'http://localhost:3000'
     useEffect(() => {
         fetch(`${url}/api/user/getAllUsers`, {
             method: "GET",
@@ -16,59 +16,55 @@ const url = 'http://localhost:3000'
                 "Content-Type": "application/json"
             }
         }).then(resp => resp.json())
-            .then(data => setUsuarios(data), tamanho =usuarios.length)
+            .then(data => setUsuarios(data), tamanho = usuarios.length)
             .catch(err => console.error(err))
-    },[{}])
+    }, [{}])
 
-function cadastrar(){ 
-    let achado = false
-    for(let i=0; i<tamanho; i++){
-        console.log(usuarios[i].email+ "   :" + document.getElementById("email").value)
-        if(usuarios[i].email == document.getElementById("email").value){
-            
-            achado = true
-            
+    function cadastrar() {
+        let achado = false
+        for (let i = 0; i < tamanho; i++) {
+            console.log(usuarios[i].email + "   :" + document.getElementById("email").value)
+            if (usuarios[i].email == document.getElementById("email").value) {
+
+                achado = true
+
+            }
         }
-    }
-    if(achado == false){
-        console.log("n existe")
+        if (achado == false) {
+            console.log("n existe")
 
-    
+
             fetch(`${url}/api/user/registerUser`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-    
+
                 body: JSON.stringify({
                     "nome": document.getElementById("nome").value,
                     "email": document.getElementById("email").value,
                     "senha": document.getElementById("senha").value,
                     "admin": "true"
-    
+
                 },
-                setTypeMessage("success"),
-                setMessage("Usuário cadastrado com sucesso!")),
-                
+                    setTypeMessage("success"),
+                    setMessage("Usuário cadastrado com sucesso!")),
+
             }).catch(err => console.error(err))
-            
-        
-    }else{ 
-        setTypeMessage("error")
-        let string = "Esse usuário já é cadastrado no sistema"
-        
-        setMessage(string);
+
+
+        } else {
+            setTypeMessage("error")
+            let string = "Esse usuário já é cadastrado no sistema"
+
+            setMessage(string);
+        }
+
     }
-    
-}
     return (
         <>
-        <div className='teste'>
             {message && <Message type={typeMessage} message={message} />}
-            <CompCadastroUser event={cadastrar}/>
-        </div>
-        
-           
+            <CompCadastroUser event={cadastrar} />
         </>
     )
 }
