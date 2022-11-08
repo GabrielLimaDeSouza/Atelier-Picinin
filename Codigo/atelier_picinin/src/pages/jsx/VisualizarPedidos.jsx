fe100cacd3e366e505e0d660a87e0b420322ec77
GabrieldeSouza1 <gabriel.souza.1365691@sga.pucminas.br>
2022-11-07 21:39:56 -0300

    {Update} Visualizar itens do pedido

--- Codigo/atelier_picinin/src/pages/jsx/VisualizarPedidos.jsx
+++ Codigo/atelier_picinin/src/pages/jsx/VisualizarPedidos.jsx
@@ -2,51 +2,50 @@
 import '../css/visualizarPedidos/visualizarPedidos.css'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { BiTrash, BiPencil } from 'react-icons/bi'
+import { useState, useEffect } from "react"
 const VisualizarPedidos = () => {
-    var teste = [{
-        usuario: "teste",
-        dataDeEntrega: "20/01/2023",
-        limiteDePagamento: "20/11/2022",
-        status: "vencendo"
-    },
-    {
-        usuario: "teste1",
-        dataDeEntrega: "20/01/2023",
-        limiteDePagamento: "20/12/2022",
-        status: "pago"
-    },
-    {
-        usuario: "teste2",
-        dataDeEntrega: "20/01/2023",
-        limiteDePagamento: "20/10/2022",
-        status: "expirado"
-    }
-    ]
+    const [pedidos, setPedidos] = useState([])
+
+    const url = 'http://localhost:3000'
+    fetch(`${url}/api/order/getAllOrders`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(resp => resp.json())
+        .then(data => setPedidos(data))
     var prioridade = []
     var expirado = []
     var vencendo = []
     var pago = []
-
-    for (let i = 0; i < teste.length; i++) {
-        if (teste[i].status == "expirado") {
-            expirado.push(teste[i]);
-        } else {
-            if (teste[i].status == "vencendo") {
-                vencendo.push(teste[i])
-            }
-            else {
-                pago.push(teste[i])
+    if (pedidos.length != 0) {
+        for (let i = 0; i < pedidos.length; i++) {
+            if (pedidos[i].status == "expirado") {
+                expirado.push(pedidos[i]);
+            } else {
+                if (pedidos[i].status == "vencendo") {
+                    vencendo.push(pedidos[i])
+                }
+                else {
+                    pago.push(pedidos[i])
+                }
             }
         }
+
+
+        if (expirado.length != 0) {
+            prioridade.push(expirado)
+        }
+        if (vencendo.length != 0) {
+            prioridade.push(vencendo)
+        }
+        if (pago.length != 0) {
+            prioridade.push(pago)
+        }
+        prioridade = prioridade[0]
+        console.log(prioridade.length)
     }
 
-
-
-    prioridade.push(expirado)
-    prioridade.push(vencendo)
-    prioridade.push(pago)
-
-    console.log(prioridade[0][0])
     return (
         <>
             <div className="divTabela">
@@ -65,24 +64,47 @@ const VisualizarPedidos = () => {
                         <tbody>
                             {prioridade.map(element => (
                                 <><tr>
-                                    <td>{element[0].usuario}</td>
-                                    <td><button className="carrinho"><AiOutlineShoppingCart/></button></td>
-                                    <td>{element[0].dataDeEntrega}</td>
-                                    <td>{element[0].limiteDePagamento}</td>
+                                    <td>{element.idCliente}</td>
+                                    <td><button type="button" data-bs-toggle="modal" data-bs-target={"#exampleModal" + element._id}>
+                                        <AiOutlineShoppingCart /></button></td>
+                                    <td>{element.dataDeEntrega}</td>
+                                    <td>{element.limiteDePagamento}</td>
                                     <td>{
-                                        element[0].status == "pago" ? <td><span className='_ok_3m2o5_87'>PAGO</span></td> : element[0].status == "vencendo" ? <td><span className='_vencendo_3m2o5_107'>VENCENDO</span></td> : <td><span className='_emfalta_3m2o5_67'>VENCIDO</span></td>
+                                        element.status == "pago" ? <td><span className='_ok_3m2o5_87'>PAGO</span></td> : element.status == "vencendo" ? <td><span className='_vencendo_3m2o5_107'>VENCENDO</span></td> : <td><span className='_vencendo_3m2o5_107'>PENDENTE</span></td>
                                     }</td>
                                     <td><BiTrash /></td>
                                 </tr>
+                                    <div class="modal fade" id={"exampleModal"+ element._id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    {element.cartItems.map(itensCarrinho => <>
+                                                    <div className='cardVisualizar'>
+                                                        <div className='teste75'>
+                                                        <div>Produto: {(itensCarrinho.nome)} x{(itensCarrinho.quantidade)} <br /> <p className='small'>{itensCarrinho.sabor}</p></div>
+                                                        <div>Preço: {(itensCarrinho.preco)}</div>
+                                                        </div>
+                                                        
+                                                    </div></>)}
+                                                    
+                                                </div>
+                                                <div class="modal-footer">
+                                                    Preço total: {element.total}
+                                            
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </>
                             ))}
+
                         </tbody>
                     </table>
-                    <div>
-                        {prioridade.map(element => (
-                            element.usuario
-                        ))}
-                    </div>
                 </>
             </div>
         </>
